0120d1ee8b85e5d2dad4aee5b52c31127a570e5f
Claudia Maganhi <claudia.maganhi@gmail.com>
2020-05-13 14:14:24 -0300

    First Commit

---
+++ FlashChat/FlashChat/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Claudia Cavalini Maganhi on 13/05/20.
+//  Copyright © 2020 Claudia Cavalini Maganhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
